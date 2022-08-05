5e16ee43230158196b1ec545e3e2521b85e769e9
bijoy862 <bijoydebnath862@gmail.com>
2022-08-05 17:34:42 +0600

    image added

--- trgrp/add_package.php
+++ trgrp/add_package.php
@@ -1,87 +1,115 @@
 <?php
-	session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Tour Group Dashboard</title>
-	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
-	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
-  	<script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
-  	<script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
-  	<style type="text/css">
-  		#side_bar{
-  			background-color: whitesmoke;
-  			padding: 50px;
-  			width: 300px;
-  			height: 450px;
-  		}
-  	</style>
+  <title>Tour Group Dashboard</title>
+  <meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+  <script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+  <script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+    #side_bar {
+      background-color: whitesmoke;
+      padding: 50px;
+      width: 300px;
+      height: 450px;
+    }
+  </style>
 </head>
+
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="TG_dashboard.php">Bijoy Travels</a>
-			</div>
-			<font style="color: white"><span><strong>Welcome: <?php echo $_SESSION['name'];?></strong></span></font>
-			<font style="color: white"><span><strong>Email: <?php echo $_SESSION['email'];?></strong></span></font>
-			<ul class="nav navbar-nav navbar-right">
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" data-toggle="dropdown">My Profile</a>
-					<div class="dropdown-menu">
-						<a class="dropdown-item" href="view_profile.php">View Profile</a>
-						<a class="dropdown-item" href="edit_profile.php"> Edit Profile</a>
-						<a class="dropdown-item" href="change_password.php">Change Password</a>
-					</div>
-				</li>
-				<li class="nav-item"><a class="nav-link" href="../logout.php">Logout</a></li>
-			</ul>
-		</div>
-	</nav>
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd">
-	<div class="container-fluid">
-		<ul class="nav navbar-nav navbar-center">
-			<li class="nav-item">
-				<a href="TG_dashboard.php" class="nav-link">Dashboard</a>
-			</li>
-			
-		</ul>
-	</div>
-</nav>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="TG_dashboard.php">Bijoy Travels</a>
+      </div>
+      <font style="color: white"><span><strong>Welcome: <?php echo $_SESSION['name']; ?></strong></span></font>
+      <font style="color: white"><span><strong>Email: <?php echo $_SESSION['email']; ?></strong></span></font>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown">My Profile</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="view_profile.php">View Profile</a>
+            <a class="dropdown-item" href="edit_profile.php"> Edit Profile</a>
+            <a class="dropdown-item" href="change_password.php">Change Password</a>
+          </div>
+        </li>
+        <li class="nav-item"><a class="nav-link" href="../logout.php">Logout</a></li>
+      </ul>
+    </div>
+  </nav>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd">
+    <div class="container-fluid">
+      <ul class="nav navbar-nav navbar-center">
+        <li class="nav-item">
+          <a href="TG_dashboard.php" class="nav-link">Dashboard</a>
+        </li>
 
-	<br><br>
-	<div class="row">
-		<div class="col-md-4"></div>
-		<div class="col-md-4">
-			<form action="" method="post">
-				<div class="form-group">
-					<label>Package Name:</label>
-					<input type="text" name="package_name" class="form-control" required="">
-				</div>
-				<div class="form-group">
-					<label>Tour Group:</label>
-					<input type="text" name="tg" class="form-control" value = "<?php echo $_SESSION['id'];?>" required="">
-				</div>
+      </ul>
+    </div>
+  </nav>
 
-				<div class="form-group">
-					<label>package Price:</label>
-					<input type="text" name="package_price" class="form-control" required="">
-				</div>
-				<button class="btn btn-primary" name="add_package">Add package</button>
+  <br><br>
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+      <form action="" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+          <label>Package Name:</label>
+          <input type="text" name="package_name" class="form-control" required="">
+        </div>
+        <div class="form-group">
+          <label>Tour Group:</label>
+          <input type="text" name="tg" class="form-control" value="<?php echo $_SESSION['id']; ?>" required="">
+        </div>
 
-			</form>
-		</div>
-		<div class="col-md-4"></div>
-	</div>
+        <div class="form-group">
+          <label>package Price:</label>
+          <input type="text" name="package_price" class="form-control" required="">
+        </div>
+
+        <div class="form-group">
+          <label>Image</label>
+          <input type="file" name="package_image" id="package_image" class="form-control">
+        </div>
+        <button class="btn btn-primary" name="add_package">Add package</button>
+
+      </form>
+    </div>
+    <div class="col-md-4"></div>
+  </div>
 </body>
+
 </html>
 
 <?php
-	if(isset($_POST['add_package'])){
-		$connection = mysqli_connect("localhost","root","");
-		$db = mysqli_select_db($connection,"tour");
-		$query = "insert into packages values(null,'$_POST[package_name]','$_POST[tg]',$_POST[package_price])";
-		$query_run = mysqli_query($connection,$query);
-	}
+if (isset($_POST['add_package'])) {
+
+  $package_name = $_POST['package_name'];
+  $tg = $_POST['tg'];
+  $package_price = $_POST['package_price'];
+
+  $img_name = $_FILES['package_image']['name'];
+
+  //echo $img_name;
+  //Extracting image extension
+  $image_extension = pathinfo($img_name, PATHINFO_EXTENSION);
+  //changing the image name unique
+  $package_image = 'image_' . date("mjYHis") . '.' . $image_extension;
+  //moving the image into folder
+  move_uploaded_file($_FILES['package_image']['tmp_name'], '../images/' . $package_image);
+
+  $connection = mysqli_connect("localhost", "root", "");
+  $db = mysqli_select_db($connection, "tour");
+  // $query = "insert into packages values(null,'$_POST[package_name]','$_POST[tg]',$_POST[package_price],'$package_image')";
+
+  $query = "INSERT INTO packages (package_name, tg_id, package_price, package_image) 
+  VALUES ('$package_name', '$tg', '$package_price', '$package_image');";
+
+
+  $query_run = mysqli_query($connection, $query);
+}
 ?>
